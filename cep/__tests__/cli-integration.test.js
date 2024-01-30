74eb84fbdea020c27e14c93e816f846408a52d17
João Victor Vilela dos Santos <148297412+vilela19@users.noreply.github.com>
2024-01-30 07:31:59 -0300

    Update cli-integration.test.js

--- cep/__tests__/cli-integration.test.js
+++ cep/__tests__/cli-integration.test.js
@@ -9,8 +9,3 @@ test('outputs version', async () => {
   const output = await cli('--version')
   expect(output).toContain('0.0.1')
 })
-
-test('outputs help', async () => {
-  const output = await cli('--help')
-  expect(output).toContain('0.0.1')
-})
